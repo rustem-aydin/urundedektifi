dd84276e4ff70c2e5540e83941e5faa6392a6efd
rustem <rustemaydinnn@gmail.com>
2026-06-10 20:03:46 +0300

    sdasd

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,8 +1,6 @@
-'use client'
-import dynamic from 'next/dynamic'
+import BarcodeScanner from '@/components/BarcodeScanner'
 
 // Dinamik import ile SSR'ı devre dışı bırak
-const BarcodeScanner = dynamic(() => import('@/components/BarcodeScanner'), { ssr: false })
 
 export default function TaraPage() {
   return (
