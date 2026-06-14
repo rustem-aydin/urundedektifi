ef7e51660f65a786e8ef17efe6dbd1f1b701d427
rustem <rustemaydinnn@gmail.com>
2026-06-14 20:00:19 +0300

    asd

--- src/collections/Countries.ts
+++ src/collections/Countries.ts
@@ -86,4 +86,23 @@ export const Countries: CollectionConfig = {
       },
     },
   ],
+  hooks: {
+    beforeValidate: [
+      ({ data, operation }) => {
+        if (operation === 'create' && data?.name && !data.slug) {
+          data.slug = data.name
+            .toLowerCase()
+            .replace(/ğ/g, 'g')
+            .replace(/ü/g, 'u')
+            .replace(/ş/g, 's')
+            .replace(/ı/g, 'i')
+            .replace(/ö/g, 'o')
+            .replace(/ç/g, 'c')
+            .replace(/[^a-z0-9]+/g, '-')
+            .replace(/(^-|-$)/g, '')
+        }
+        return data
+      },
+    ],
+  },
 }
