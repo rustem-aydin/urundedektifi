4e104c2f1605b87ccdd8af5aeac9bf89c70a44d1
rustem <rustemaydinnn@gmail.com>
2026-06-22 19:59:23 +0300

    up

---
+++ src/migrations/20260622_165857_fix_allergens_schema.ts
@@ -0,0 +1,13 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "ingredients" ADD COLUMN "slug" varchar;
+  CREATE UNIQUE INDEX "ingredients_slug_idx" ON "ingredients" USING btree ("slug");`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   DROP INDEX "ingredients_slug_idx";
+  ALTER TABLE "ingredients" DROP COLUMN "slug";`)
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,7 @@
 import * as migration_20260613_084314 from './20260613_084314';
 import * as migration_20260622_162900_fix_allergens_schema from './20260622_162900_fix_allergens_schema';
+import * as migration_20260622_165857_fix_allergens_schema from './20260622_165857_fix_allergens_schema';
+import * as migration_20260622_165913_fix_allergens_schema2 from './20260622_165913_fix_allergens_schema2';
 
 export const migrations = [
   {
@@ -10,6 +12,16 @@ export const migrations = [
   {
     up: migration_20260622_162900_fix_allergens_schema.up,
     down: migration_20260622_162900_fix_allergens_schema.down,
-    name: '20260622_162900_fix_allergens_schema'
+    name: '20260622_162900_fix_allergens_schema',
+  },
+  {
+    up: migration_20260622_165857_fix_allergens_schema.up,
+    down: migration_20260622_165857_fix_allergens_schema.down,
+    name: '20260622_165857_fix_allergens_schema',
+  },
+  {
+    up: migration_20260622_165913_fix_allergens_schema2.up,
+    down: migration_20260622_165913_fix_allergens_schema2.down,
+    name: '20260622_165913_fix_allergens_schema2'
   },
 ];
